f594de8cadb44d6d37d0a7571bab4637a60b1b6e
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-31 11:27:14 +0700

    add method toString

--- src/main/java/com/codegym/cms/model/Customer.java
+++ src/main/java/com/codegym/cms/model/Customer.java
@@ -7,12 +7,21 @@ import javax.persistence.*;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
 
-    public Customer() {
+    public Customer() {}
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
     }
 
     public Long getId() {
