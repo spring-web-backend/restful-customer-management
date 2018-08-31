8b3d85d548d165ad66dbaed37784dcd085e75b24
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-31 11:28:17 +0700

    clean up code

--- src/main/java/com/codegym/cms/repository/Repository.java
+++ src/main/java/com/codegym/cms/repository/Repository.java
@@ -1,10 +1,8 @@
 package com.codegym.cms.repository;
 
-import com.codegym.cms.model.Customer;
-
 import java.util.List;
 
-public interface Repository<T> {
+public interface Repository <T> {
     List<T> findAll();
 
     T findById(Long id);
@@ -13,4 +11,3 @@ public interface Repository<T> {
 
     void remove(Long id);
 }
-
